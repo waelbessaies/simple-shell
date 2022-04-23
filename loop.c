acc0c753983b00d29b39ea002c915e83f0ec0e5f
wael <waelbessaies2@gmail.com>
2022-04-23 13:14:41 +0100

    simple shell

---
+++ loop.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+/**
+* main - main function to call the loop
+*
+* Return: 0 success, other otherwise
+**/
+int main(int argc, char **argv)
+{
+
+	loop();
+
+	return (0);
+}
+
---
+++ prompt.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+/**
+ * prompt - displays prompt and waits for input
+ *
+ * Return: VOID
+ */
+void prompt(void)
+{
+
+	write(STDOUT_FILENO, $, 2);
+}
